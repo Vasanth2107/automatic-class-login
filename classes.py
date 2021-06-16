2bfa3abf10326def6019bb0f4324ec021c35abad
Vasanth2107 <66245550+Vasanth2107@users.noreply.github.com>
2021-06-16 23:52:01 +0530

    Update classes.py

--- classes.py
+++ classes.py
@@ -7,52 +7,52 @@ def open_link(link):
     webbrowser.open(link)
 
 
-def dsd():
+def class1():
     print("running")
-    open_link('https://us04web.zoom.us/j/79599887604?pwd=OUNhSGJ6dmU1dlJaejdrelhzREduQT09')
-def adc():
+    open_link('link')
+def class2():
     print("running")
-    open_link('https://us04web.zoom.us/j/74871911316?pwd=T3AzYmVjSW5FTjE1R0NuclJlWXBKZz09')
-def cs():
+    open_link('link')
+def class3():
     print("running")
-    open_link('https://us04web.zoom.us/j/6657325252?pwd=M1ozSG4vTjgwNStoRVZlKzZyc0lqUT09')
-def emt():
+    open_link('link')
+def class4():
     print("running")
-    open_link('https://us04web.zoom.us/j/4051632980?pwd=MmZrWGw2UnMyQ3VHUWtBcFV0Sk1Xdz09')
-def ac():
+    open_link('link')
+def class5():
     print("running")
-    open_link('https://us04web.zoom.us/j/5546372849?pwd=Qk1IOWd3bzltSlkrMjU1RElGUkVSQT09')
-def ccna():
+    open_link('link')
+def class6():
     print("running")
-    open_link('https://us04web.zoom.us/j/75841646905?pwd=L1pRVUFPTnVVcTBzZVhlbW9wcG9oZz09')
+    open_link('link')
 
 
-schedule.every().monday.at("10:05").do(dsd)
-schedule.every().monday.at("10:58").do(cs)
-schedule.every().monday.at("11:58").do(emt)
-schedule.every().monday.at("13:58").do(adc)
-schedule.every().monday.at("14:58").do(ac)
-schedule.every().tuesday.at("09:58").do(ac)
-schedule.every().tuesday.at("11:05").do(adc)
-schedule.every().tuesday.at("11:58").do(dsd)
-schedule.every().tuesday.at("14:03").do(emt)
-schedule.every().wednesday.at("10:00").do(cs)
-schedule.every().wednesday.at("11:00").do(adc)
-schedule.every().wednesday.at("11:58").do(ac)
-schedule.every().wednesday.at("13:58").do(dsd)
-schedule.every().thursday.at("10:00").do(adc)
-schedule.every().thursday.at("11:00").do(dsd)
-schedule.every().thursday.at("12:00").do(cs)
-schedule.every().thursday.at("13:58").do(emt)
-schedule.every().thursday.at("14:58").do(ac)
-schedule.every().friday.at("10:12").do(ac)
-schedule.every().friday.at("10:58").do(emt)
-schedule.every().friday.at("12:02").do(adc)
-schedule.every().friday.at("13:58").do(cs)
-schedule.every().saturday.at("10:06").do(dsd)
-schedule.every().saturday.at("10:58").do(emt)
-schedule.every().saturday.at("11:58").do(cs)
-schedule.every().saturday.at("14:45").do(ccna)
+schedule.every().monday.at("10:00").do(class1)
+schedule.every().monday.at("11:00").do(class2)
+schedule.every().monday.at("12:00").do(class3)
+schedule.every().monday.at("13:00").do(class4)
+schedule.every().monday.at("14:00").do(class5)
+schedule.every().tuesday.at("09:00").do(class1)
+schedule.every().tuesday.at("11:00").do(class2)
+schedule.every().tuesday.at("12:00").do(class3)
+schedule.every().tuesday.at("14:00").do(class4)
+schedule.every().wednesday.at("10:00").do(class5)
+schedule.every().wednesday.at("11:00").do(class4)
+schedule.every().wednesday.at("12:00").do(class3)
+schedule.every().wednesday.at("13:00").do(class1)
+schedule.every().thursday.at("10:00").do(class3)
+schedule.every().thursday.at("11:00").do(class1)
+schedule.every().thursday.at("12:00").do(class5)
+schedule.every().thursday.at("13:00").do(class2)
+schedule.every().thursday.at("14:00").do(class6)
+schedule.every().friday.at("10:00").do(class4)
+schedule.every().friday.at("11:00").do(class2)
+schedule.every().friday.at("12:00").do(class1)
+schedule.every().friday.at("13:00").do(class6)
+schedule.every().saturday.at("10:00").do(class2)
+schedule.every().saturday.at("11:00").do(class3)
+schedule.every().saturday.at("12:00").do(class4)
+schedule.every().saturday.at("14:00").do(class6)
 
 
 while 1:
